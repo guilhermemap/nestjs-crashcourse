4ef067c5724b767fd483dbc9eb239dbd8760babc
guilherme <guilhermemb11@gmail.com>
2024-03-04 22:23:27 -0300

    http localhost:3000/user/2 param from url

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
   @Get('all')
   findAll() {
     return 'all users';
+  }
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return {
+      user: {
+        id: id,
+      },
+    };
   }
 }
