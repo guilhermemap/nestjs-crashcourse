400c0bde609ad268f5c0020ac512ee7dbbdbd8c1
guilherme <guilhermemb11@gmail.com>
2024-03-04 23:30:12 -0300

    injects commentService into userController

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUserDto';
 import { UserService } from './user.service';
+import { CommentService } from 'src/comment/comment.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly commentService: CommentService,
+  ) {}
 
   @Get('all')
   findAll() {
@@ -18,5 +22,10 @@ export class UserController {
   @Post()
   create(@Body() user: CreateUserDto) {
     return this.userService.create(user);
+  }
+
+  @Get(':id/comments')
+  getUserComments(@Param('id') id: number) {
+    return this.commentService.findUserComments(id);
   }
 }
