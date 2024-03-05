99ff61e80aa36b1eaa8e87cfeac38007324d659e
guilherme <guilhermemb11@gmail.com>
2024-03-04 23:29:52 -0300

    dummy findUserComments

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+  findUserComments(userId: number) {
+    return 'these are the comments of the user with id ' + userId;
+  }
+}
