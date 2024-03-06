7cb60ef0310e1a87faff788175242245166b5f93
guilherme <guilhermemb11@gmail.com>
2024-03-05 23:28:22 -0300

    add UserRepository to UserModule

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUserDto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
   findOne(id: number) {
     return {
       id: id,
